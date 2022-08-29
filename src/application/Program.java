5ee2068f44b75952d251b9793aeabeea340dd5de
Lucas Rocha <lucasrocha070101@gmail.com>
2022-08-29 10:53:07 +0100

    Starting to implement Board, Piece, and Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,8 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Work in progress...");
 
-		System.out.println("commiting to github");
 	}
 }
