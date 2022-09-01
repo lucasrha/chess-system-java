51e9182cb4e99147b2735e511a875d7d6a86e0e5
Lucas Rocha <lucasrocha070101@gmail.com>
2022-09-01 09:19:58 +0100

    Printing chess board, and placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,17 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Work in progress...");
+		System.out.println();
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 
 	}
 }
